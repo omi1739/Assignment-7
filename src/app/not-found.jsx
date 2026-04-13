e823cb75c979c906bd71be2a3317289d320fec7a
Omi <seyam.islam020@gmail.com>
2026-04-13 20:34:20 +0600

    added daisy ui and 404 page not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+
+
+const notfound = () => {
+  return (
+    <div>
+      <h1>This Page is not found</h1>
+      <h1>404 not found</h1>
+
+    </div>
+  )
+}
+
+export default notfound
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h2 className="text-3xl">Hello OMIBOSS</h2>
+  );
+}
